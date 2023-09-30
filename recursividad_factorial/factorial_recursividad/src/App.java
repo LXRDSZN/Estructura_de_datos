15006c55298c955cc0c686ef8b41375899d0c7e3
lxrdszn <22680124@cuautla.tecnm.mx>
2023-09-30 09:40:55 -0600

    [30/sep/23---->recursividad]

---
+++ recursividad_factorial/factorial_recursividad/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        factorial Factorial = new factorial();//creacion del objeto llmado Factorial
+        Factorial.factorial();//invocacion del objeto
+    }
+}
